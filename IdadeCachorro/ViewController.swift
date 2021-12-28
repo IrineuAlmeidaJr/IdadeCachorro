008a2f15a4c3be624c4c556a27fa88d5e6834efc
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2021-12-28 01:45:24 -0300

    Commit - Fim
    Primeiro app feito em Swift

--- IdadeCachorro/ViewController.swift
+++ IdadeCachorro/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var legendaResultado: UILabel!
+        
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        let leitura = campoIdadeCachorro.text?.isEmpty
+        if leitura == true {
+            legendaResultado.text = "Digite algo. Campo vazio!"
+        } else {
+            let idade = Int(campoIdadeCachorro.text!)! * 7
+            idade > 0 ? (legendaResultado.text = "A idade do cachorro em anos humanos é: " + String(idade)) : (legendaResultado.text = "Digite idade válida. Idade maior que zero")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
